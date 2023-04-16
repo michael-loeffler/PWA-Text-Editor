3cb00bed680327097282f114e4cc96d687b544ef
mloeff <michaelloeffler23@gmail.com>
2023-04-16 11:48:36 -0500

    adjusts styling of install button to be hidden on initial render then creates scripts in install.js to unhide button if the app can be installed, prompts the user if they want to install the app, and hides the button again after successful installation.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## Credits
 
-*Scripts included in the package.json file for the main directory and asset-caching in src-sw.js are borrowed from course activities*
+* *Scripts included in the package.json file for the main directory and asset-caching in src-sw.js are borrowed from course activities*
+* *Scripts for install.js file follow the guides included in the very helpful articles [How to provide your own in-app install experience](https://web.dev/customize-install/) and [Make it installable](https://web.dev/codelab-make-installable/).*
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,11 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+
+    console.log("beforeinstallprompt event triggered!");
+    window.deferredPrompt = event;
+    butInstall.style.display = "inline-block";
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    console.log("Install button clicked!")
+    window.deferredPrompt.prompt();
+    const { outcome } = await window.deferredPrompt.userChoice;
+    if (outcome === 'accepted') {
+        window.deferredPrompt = null;
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
-//hide button & confirm installation
+window.addEventListener('appinstalled', (event) => {
+    console.log("Just Another Text Editor has been successfully installed!")
+    butInstall.style.display = "none";
+    window.deferredPrompt = null;
+});
+// when is this fired? app automatically opens in a new window and closes browser tab-- is that an issue?
